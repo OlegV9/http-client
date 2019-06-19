deca858cc836cbfeaca645c7d6485ae9c94f42de
Oleg V <olegv@semalt.com>
2019-06-19 11:34:58 +0300

    Initial commit

---
+++ HttpClient.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace HttpClient;
+
+class HttpClient {
+	private $defaultOpts = [
+		'type' => 'form',
+		'followRedirects' => true,
+		'maxRedirects' => 10,
+		'ignoreSslErrors' => false,
+		'timeout' => 40
+	];
+
+	public function __construct($defOpts = null) {
+		if (is_array($defOpts)) {
+			$this->defaultOpts = array_merge($this->defaultOpts, $defOpts);
+		}
+	}
+
+	public function get($url, $opts = []) {
+		return $this->makeRequest('GET', $url, null, $opts);
+	}
+
+	public function post($url, $data = [], $opts = []) {
+		return $this->makeRequest('POST', $url, $data, $opts);
+	}
+
+	public function put($url, $data = [], $opts = []) {
+		return $this->makeRequest('PUT', $url, $data, $opts);
+	}
+
+	public function delete($url, $data = [], $opts = []) {
+		return $this->makeRequest('DELETE', $url, $data, $opts);
+	}
+
+	public function set($key, $val) {
+		$this->defaultOpts[$key] = $val;
+	}
+
+	private function makeRequest($method, $url, $postData = null, $opts = []) {
+		$opts = array_merge($this->defaultOpts, $opts);
+		$headers = [];
+
+		$readHeaders = function($ch, $header) use (&$headers) {
+			$len = strlen($header);
+			$parts = explode(':', $header, 2);
+			if (count($parts) < 2) return $len;
+
+			list($key, $val) = $parts;
+
+			$headers[] = [
+				'key' => mb_strtolower(trim($key)),
+				'val' => trim($val)
+			];
+
+			return $len;
+		};
+
+		$reqHeaders = [];
+
+		$ch = curl_init($url);
+		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $method);
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($ch, CURLOPT_HEADERFUNCTION, $readHeaders);
+
+		if ($postData) {
+			$postData = $this->transformPostData($postData, $opts['type']);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);
+
+			if ($opts['type'] === 'json') {
+				$reqHeaders['content-type'] = 'application/json';
+			}
+		}
+
+		if (!empty($opts['ignoreSslErrors'])) {
+			curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+			curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+		}
+		if (isset($opts['maxRedirects'])) {
+			curl_setopt($ch, CURLOPT_MAXREDIRS, intval($opts['maxRedirects']));
+		}
+		if (!empty($opts['followRedirects'])) {
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		}
+		if (isset($opts['timeout'])) {
+			curl_setopt($ch, CURLOPT_TIMEOUT, $opts['timeout']);
+		}
+		if (isset($opts['auth'])) {
+			curl_setopt($ch, CURLOPT_USERPWD, $opts['auth']);
+		}
+		if (!empty($opts['headers'])) {
+			foreach ($opts['headers'] as $key => $val) {
+				$header = strtolower($key);
+				$reqHeaders[$header] = $val;
+			}
+		}
+
+		if (!empty($reqHeaders)) {
+			$headersList = [];
+			foreach ($reqHeaders as $key => $val) {
+				$headersList[] = $key.': '.$val;
+			}
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headersList);
+		}
+
+		$response = curl_exec($ch);
+		$error = curl_error($ch);
+		$info = curl_getinfo($ch);
+		curl_close($ch);
+
+		return new HttpClientResult($response, $headers, $info, $error);
+	}
+
+	private function transformPostData($data, $type) {
+		if ($type === 'json') return json_encode($data, JSON_UNESCAPED_UNICODE);
+		if ($type === 'form') return http_build_query($data);
+		return $data;
+	}
+}
---
+++ HttpClientResult.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace HttpClient;
+
+class HttpClientResult {
+	private $response = '';
+	private $error = '';
+	private $headers;
+	private $info;
+
+	public function __construct($response, $headers, $info, $error) {
+		$this->response = $response;
+		$this->error = $error;
+		$this->headers = $headers;
+		$this->info = $info;
+	}
+
+	public function getText() : string {
+		return $this->response;
+	}
+
+	public function fromJson($assoc = false) {
+		return json_decode($this->response, $assoc);
+	}
+
+	public function toFile($path) {
+		if (file_exists($path)) {
+			unlink($path);
+		}
+		$ok = !!file_put_contents($path, $this->response);
+		return $ok;
+	}
+
+	public function getCode() : int {
+		return $this->info['http_code'] ?? 0;
+	}
+
+	public function getError() : string {
+		return $this->error;
+	}
+
+	public function getAllHeaders() : array {
+		return $this->headers;
+	}
+
+	public function getHeader(string $key) : string {
+		$key = mb_strtolower($key);
+		foreach ($this->headers as $header) {
+			if ($header['key'] === $key) return $header['val'];
+		}
+
+		return null;
+	}
+
+	public function getUrl() : string {
+		return $this->info['url'];
+	}
+
+	/* aliases */
+
+	public function text() : string {
+		return $this->getText();
+	}
+
+	public function json($assoc = false) {
+		return $this->fromJson($assoc);
+	}
+
+	public function code() : int {
+		return $this->getCode();
+	}
+
+	public function error() : string {
+		return $this->getError();
+	}
+}
